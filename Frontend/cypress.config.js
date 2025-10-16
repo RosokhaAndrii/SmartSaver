81a18cee31f19f37dad42155c9de8b962c45c920
RosokhaAndrii <rosokha.andrii@student.uzhnu.edu.ua>
2025-10-16 12:08:31 +0300

    Arcitecture

---
+++ Frontend/cypress.config.js
@@ -0,0 +1,14 @@
+export default {
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
